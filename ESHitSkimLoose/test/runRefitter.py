c486e18393a9086c4f7aea8d28e67cf9041dac0f
Alpha <jui-fa.tsai@cern.ch>
2014-12-08 17:10:12 +0100

    Update refitter

--- ESHitSkimLoose/test/runRefitter.py
+++ ESHitSkimLoose/test/runRefitter.py
@@ -7,7 +7,7 @@ process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cf
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
 ########## Change to new global tag ###############
-#process.GlobalTag.globaltag = 'GR_P_V14::All'
+process.GlobalTag.globaltag = 'POSTLS170_V6::All'
 #process.newTKAlignment = cms.ESSource("PoolDBESSource",
 #                                        process.CondDBSetup,
 #                                        connect = cms.string('frontier://FrontierProd/CMS_COND_31X_ALIGNMENT'),
@@ -28,27 +28,30 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 #                                         )
 #process.es_prefer_GlobalPositionDB = cms.ESPrefer("PoolDBESSource", "newGlobalPosition")
 
-
 process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
-process.TrackRefitter.constraint = ""
-#process.TrackRefitter.src = "doConstraint"
- 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
-)
+################### Input file #############################
+from inputFiles_cfi import * #FileNames
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-	'file:JET2011A_ESSkim_P1.root'
-  )
+    #fileNames = cms.untracked.vstring(FileNames)
+    #fileNames = cms.untracked.vstring(FileNames_SkimPionGun)
+    fileNames = cms.untracked.vstring(FileNames_PionGunTest)
+    #fileNames = cms.untracked.vstring('file:JET2011A_ESSkim_P1.root')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(2)
 )
 
+################### Define process #########################
+#process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
+#process.TrackRefitter.constraint = ""
+#process.TrackRefitter.src = "doConstraint"
 process.out = cms.OutputModule("PoolOutputModule",
      outputCommands = cms.untracked.vstring('keep *'),
-     fileName = cms.untracked.string('ESSkim_wRefitter.root')
+     fileName = cms.untracked.string('Refitter.root')
 )
 
 process.p = cms.EndPath(process.out)
-#process.p1 = cms.Path(process.doConstraint * process.TrackRefitter} 
+#process.p1 = cms.Path(process.doConstraint * process.TrackRefitter) 
 process.p1 = cms.Path(process.TrackRefitter) 
 process.schedule = cms.Schedule( process.p1 , process.p)
 
